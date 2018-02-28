4b5b91b4f917b372b930bde773a2a9f5f1fe321d
Diogo Areas <dareas@stone.com.br>
2018-02-28 19:41:29 -0300

    Add project files.

---
+++ PanaderiaRoom/Controllers/SecurityController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Security.Claims;
+using System.Security.Principal;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PanaderiaRoom.Controllers {
+    public class SecurityController : Controller {
+        public IActionResult Index() {
+            return View();
+        }
+
+        public IActionResult LogOn(string username, string password) {
+            return RedirectToAction("Index", "Home");
+        }
+
+        public IActionResult SocialLogin(string socialType) {
+            var claims = new Claim[]
+            {
+                new Claim(ClaimTypes.Name, "Social Login"),
+                new Claim(ClaimTypes.Role, "ADM"),
+                new Claim(ClaimTypes.Authentication, socialType),
+            };
+
+            HttpContext.User.AddIdentity(new GenericIdentity(socialType, "Default"));
+
+            FormsAuthenticationTicket authTicket = new FormsAuthenticationTicket(
+                1,
+                socialType,
+                DateTime.Now,
+                DateTime.Now.AddMinutes(15),
+                false,
+                new {});
+            
+
+            if (socialType == "GIM") {
+
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        public IActionResult LogOut() {
+            return RedirectToAction("Index", "Security");
+        }
+    }
+
+    public class FormsAuthenticationTicket
+    {
+        public FormsAuthenticationTicket(int i, object email, DateTime now, DateTime addMinutes, bool b, object userData)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ PanaderiaRoom/wwwroot/js/gim.js
@@ -0,0 +1,82 @@
+﻿var GlobalIdentity = (function () {
+    'use strict';
+
+    GIM = window.GIM || (window.GIM = []);
+
+    /*_executeRequest.*/
+
+    var _executeRequest = function (spec) {
+        var url = spec.url || '';
+        var method = spec.method || '';
+        var headers = spec.headers || {};
+        var params = spec.params || undefined;
+        var successCallback = spec.successCallback || function () { };
+        var errorCallback = spec.errorCallback || function () { };
+        var headersKeys = Object.keys(headers);
+        var xhr = new XMLHttpRequest();
+
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState == 4) {
+                var resText = xhr.responseText || '{}';
+                var response = JSON.parse(resText);
+
+                if ((xhr.status == 200 || xhr.status == 201)) {
+                    successCallback(response);
+                } else {
+                    errorCallback(response);
+                }
+            }
+        };
+
+        xhr.open(method, url, true);
+
+        for (var i = 0; i < headersKeys.length; i++) {
+            var key = headersKeys[i];
+            xhr.setRequestHeader(key, headers[key]);
+        }
+
+        xhr.withCredentials = true;
+        xhr.send(params);
+    };
+
+    var _validateSession = function (successCallback, errorCallback) {
+        var spec = {
+            url: 'http://172.16.2.160:8000/api/Authenticate/ValidateSessionId',
+            method: 'GET',
+            successCallback: successCallback,
+            errorCallback: errorCallback,
+            headers: {
+                'Accept': 'application/json',
+                'Content-type': 'application/json'
+            }
+        };
+
+        _executeRequest(spec);
+    }
+
+    GIM.checkLoginStatus = function (clientid, redirecturi, scopes, responsetype) {
+        var loginURI = 'http://172.16.2.160:8000/?clientid=' + clientid + '&redirecturi=' + redirecturi + '&scopes=' + scopes + '&responsetype=' + responsetype + '';
+
+        return new Promise(function (resolve, reject) {
+            _validateSession(
+                function (e) {
+                    console.log('=> ', e);
+                },
+                function (e) {
+                    //console.log(e.operation_reports[0].message);
+
+                    resolve(
+                        {
+                            'errorMessage': "e.operation_reports[0].message",
+                            'urlLogin': loginURI
+                        }
+                    );
+                });
+        });
+    };
+
+})();
+
+if (window.gimInit) {
+    window.gimInit();
+}
